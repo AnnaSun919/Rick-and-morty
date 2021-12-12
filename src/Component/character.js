41c60b1a6d2680645a698168795c0e8aeebf1f0d
annasun919 <yuval_sun@yahoo.com.hk>
2021-12-12 20:38:07 +0100

    set page

--- src/Component/character.js
+++ src/Component/character.js
@@ -4,6 +4,7 @@ import { API_URL } from "../config";
 import "./chracter.css";
 
 function Character() {
+  let [totalPage, setTotalPage] = useState(null);
   let [page, setPage] = useState(1);
   const [character, setCharacter] = useState(null);
   let [serachItem, setSeachItem] = useState({
@@ -12,41 +13,54 @@ function Character() {
     name: "",
   });
 
+  // const checkPage = () => {};
+
   useEffect(() => {
     try {
       async function getcharacter() {
         let response = await axios.get(`${API_URL}/character/?page=${page}`);
         if (serachItem) {
-          let searchValue = Object.values(serachItem);
-
-          if (serachItem["status"] && serachItem["species"]) {
-            searchValue = serachItem["name"];
-            console.log(serachItem["status"]);
+          if (
+            serachItem["status"] ||
+            serachItem["species"] ||
+            serachItem["name"]
+          ) {
+            let searchValue = serachItem["species"];
             let searchValue1 = serachItem["status"];
+            let searchValue2 = serachItem["name"];
             response = await axios.get(
-              `${API_URL}/character/?page=${page}&status=${searchValue1}&species=${searchValue}`
-            );
-          } else if (serachItem["species"]) {
-            searchValue = serachItem["species"];
-            response = await axios.get(
-              `${API_URL}/character/?page=${page}&species=${searchValue}`
-            );
-          } else if (serachItem["status"]) {
-            searchValue = serachItem["status"];
-            response = await axios.get(
-              `${API_URL}/character/?page=${page}&status=${searchValue}`
+              `${API_URL}/character/?page=${page}&status=${searchValue1}&species=${searchValue}&name=${searchValue2}`
             );
           }
+          // else if (serachItem["species"]) {
+          //   searchValue = serachItem["species"];
+          //   response = await axios.get(
+          //     `${API_URL}/character/?page=${page}&species=${searchValue}`
+          //   );
+          // } else if (serachItem["status"]) {
+          //   searchValue = serachItem["status"];
+          //   response = await axios.get(
+          //     `${API_URL}/character/?page=${page}&status=${searchValue}`
+          //   );
+          // } else if (serachItem["name"]) {
+          //   searchValue = serachItem["name"];
+          //   response = await axios.get(
+          //     `${API_URL}/character/?page=${page}&name=${searchValue}`
+          //   );
+          // }
         }
-
-        setCharacter(response.data.results);
+        console.log(response.data.info.pages);
+        setTotalPage(response.data.info.pages);
+        setCharacter(response.data.results.slice(0, 10));
       }
       getcharacter();
     } catch (err) {}
   }, [page, serachItem]);
 
-  function changePage() {
-    setPage((page += 1));
+  function changePage(event, direction) {
+    event.preventDefault();
+
+    direction === "before" ? setPage((page -= 1)) : setPage((page += 1));
   }
 
   const handleSearch = (event) => {
@@ -65,12 +79,12 @@ function Character() {
         name="species"
         onChange={(e) => handleSearch(e, "species")}
       />
-      {/* Name
+      Name
       <input
         placeholder="Filter Name"
         name="name"
         onChange={(e) => handleSearch(e, "name")}
-      /> */}
+      />
       Status
       <input
         placeholder="Filter Status"
@@ -101,7 +115,9 @@ function Character() {
           </>
         )} */}
       </grid>
-      <button onClick={changePage}>{page}</button>
+      <button onClick={(e) => changePage(e, "before")}>-</button>
+      <span>{page}</span>
+      <button onClick={(e) => changePage(e, "after")}>+</button>
     </>
   );
 }
