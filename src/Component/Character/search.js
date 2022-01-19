4b35c05715d922a024e775472fe6c91e9b34552a
annasun919 <yuval_sun@yahoo.com.hk>
2022-01-19 14:19:02 +0100

    adjust search bar

--- src/Component/Character/search.js
+++ src/Component/Character/search.js
@@ -1,49 +1,37 @@
 import React from "react";
 
 function Search(props) {
-  // const handleSearch = (event) => {
-  //   event.preventDefault();
-  //   setAPIPage(1);
-  //   setPageNo(1);
-  //   forShowItem(event, 1);
-
-  //   const { species, name, status, startDate, endDate } = event.target;
-  //   if ((startDate.value, endDate.value)) {
-  //     setDate(true);
-  //   }
-  //   if (species.value || species.value || name.value) {
-  //     setSearchOther(true);
-  //   } else if ((!startDate.value, !endDate.value)) {
-  //     setDate(false);
-  //   }
-
-  //   setSeachItem({
-  //     species: species.value,
-  //     name: name.value,
-  //     status: status.value,
-  //     startDate: startDate.value,
-  //     endDate: endDate.value,
-  //   });
-  // };
-
   return (
     <div>
       <form onSubmit={props.handleSearch}>
-        <label>Species</label>
-        <input placeholder="Filter Species" name="species" />
-        <label>Name</label>
-        <input placeholder="Filter Name" name="name" />
-        <label>Status</label>
-        <input placeholder="Filter Status" name="status" />
+        <div className="form_control">
+          <label>Species</label>
+          <input placeholder="Filter Species" name="species" />
+        </div>
+        <div className="form_control">
+          <label>Name</label>
+          <input placeholder="Filter Name" name="name" />
+        </div>
+        <div className="form_control">
+          <label>Status</label>
+          <input placeholder="Filter Status" name="status" />
+        </div>
+        <br></br>
         <br />
-        <label>Created Date: From </label>
-        <input placeholder="Filter Status" name="startDate" type="date" />
-        to
-        <input placeholder="Filter Status" name="endDate" type="date" />
-        <button type="submit">Submit</button>
-        <button type="button" onClick={props.handleClear}>
-          Clear
-        </button>
+        <div className="form_control">
+          <label>Created Date: From </label>
+          <input placeholder="Filter Status" name="startDate" type="date" />
+        </div>
+        <div className="form_control">
+          to
+          <input placeholder="Filter Status" name="endDate" type="date" />
+        </div>
+        <div className="form_control">
+          <button type="submit">Submit</button>
+          <button type="button" onClick={props.handleClear}>
+            Clear
+          </button>
+        </div>
       </form>
     </div>
   );
